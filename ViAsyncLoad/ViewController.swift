641ffc95fb7bd3539c9d8ac3938f9efe2c11df6e
Yael Bilu Eran <yael-be@yit.co.il>
2020-05-27 11:32:48 +0300

    Initial Commit

---
+++ ViAsyncLoad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViAsyncLoad
+//
+//  Created by Yael Bilu Eran on 27/05/2020.
+//  Copyright © 2020 CodeQueen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
